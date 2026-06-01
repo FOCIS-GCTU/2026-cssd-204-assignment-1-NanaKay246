858b459784e695d091f63b3dcca565d44637d6ea
Dua Nana Kwaku Boakye <2425401325@live.gctu.edu.gh>
2026-06-01 19:33:05 +0000

    Add files via upload

---
+++ intials.py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 2
+#
+# Name: NANA KWAKU BOAKYE DUA
+# STUDENT ID: 2425401325
+# Email: 2425401325@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# I promise On my honor,Nana Kwaku Boakye Dua, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    #so i've called the main function and now am going to Print out my initials NKBD as explained in the word document sent to us earlier
+
+    print()
+    print("...NKBD")
+    print()
+    print("...NN......NN.....KK.....KK......BBBBBBBBB......DDDDDDDD...........")
+    print("...NNN.....NN.....KK....KK.......BB......BB.....DD......DD.........")
+    print("...NNNN....NN.....KK...KK........BB......BB.....DD.......DD........")
+    print("...NN.NN...NN.....KK..KK.........BB......BB.....DD.......DD........")
+    print("...NN..NN..NN.....KKKKK..........BBBBBBBBB......DD.......DD........")
+    print("...NN...NN.NN.....KK..KK.........BB.......BB....DD.......DD........")
+    print("...NN....NNNN.....KK...KK........BB.......BB....DD.......DD........")
+    print("...NN.....NNN.....KK....KK.......BB.......BB....DD.......DD........")
+    print("...NN......NN.....KK.....KK......BB......BB.....DD......DD.......**")
+    print("...NN......NN.....KK.....KK......BBBBBBBBB......DDDDDDDD.........**")
+    print()
+
+
+main()
